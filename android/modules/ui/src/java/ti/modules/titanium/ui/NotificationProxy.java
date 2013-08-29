552ff60b4eeb1130a08816acea15f44b1e8cc538
Patrick Daigle <pdaigle@groupeconitech.com>
2013-08-28 20:30:47 -0400

    Fix Toast message API on Android

--- android/modules/ui/src/java/ti/modules/titanium/ui/NotificationProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/NotificationProxy.java
@@ -15,7 +15,8 @@ import org.appcelerator.titanium.view.TiUIView;
 import ti.modules.titanium.ui.widget.TiUINotification;
 import android.app.Activity;
 
-@Kroll.proxy(creatableInModule=UIModule.class)
+@Kroll.proxy(creatableInModule=UIModule.class,
+             propertyAccessors = {"message", "duration", "horizontalMargin", "verticalMargin", "offsetX", "offsetY"})
 public class NotificationProxy extends TiViewProxy
 {
 	public NotificationProxy()
