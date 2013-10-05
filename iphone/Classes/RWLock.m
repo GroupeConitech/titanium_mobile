5c6b3ee641247e44a4ad684c9ec8d434a8adc59e
Patrick Daigle <pdaigle@groupeconitech.com>
2013-10-05 16:50:39 -0400

    RWLock bug fix + improvement: - fixed seg fault because thread observer was not removed after the object was released - fixed lock was not independent (lock data was saved with thread data hence accessible by any lock object) - added debugging checks

--- iphone/Classes/RWLock.m
+++ iphone/Classes/RWLock.m
@@ -39,6 +39,9 @@
 
 #pragma mark RWLock
 
+//#define CHECK_LOCK_RELEASE // Throws an exception if the lock is not released when the thread exit
+//#define CHECK_LOCK_STATE   // Assert consistency of the ThreadLockState object
+
 @implementation RWLock
 
 - (id)init
@@ -46,13 +49,30 @@
 	if (self = [super init])
 	{
 		pthread_rwlock_init(&lock, NULL);
+		threadLockStates = [[NSMutableDictionary alloc] init];
 	}
 	return self;
+}
+
+- (void)dealloc
+{
+	[super dealloc];
+	pthread_rwlock_destroy(&lock);
+	[threadLockStates removeAllObjects];
+	[threadLockStates release];
+#ifdef CHECK_LOCK_RELEASE
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
+#endif
 }
 
 - (void)lockRead
 {
 	ThreadLockState* thread = [self currentThreadState];
+	
+#ifdef CHECK_LOCK_STATE
+	[self checkLockState:thread];
+#endif
+	
 	if (!thread->hasReadLock)
 	{
 		pthread_rwlock_rdlock(&lock);
@@ -65,7 +85,12 @@
 - (void)lockWrite
 {
 	ThreadLockState* thread = [self currentThreadState];
-	if (thread->hasReadLock)
+	
+#ifdef CHECK_LOCK_STATE
+	[self checkLockState:thread];
+#endif
+	
+	if (thread->hasReadLock && !thread->hasWriteLock)
 	{
 		[NSException raise:@"AlreadyLockedException" format:@"current thread already owns a read lock"];
 		return;
@@ -83,6 +108,11 @@
 - (void)unlock
 {
 	ThreadLockState* thread = [self currentThreadState];
+	
+#ifdef CHECK_LOCK_STATE
+	[self checkLockState:thread];
+#endif
+	
 	if (thread->lockCount <= 0)
 	{
 		[NSException raise:@"NotLockedException" format:@"current thread doesn't owns any lock"];
@@ -99,26 +129,75 @@
 	
 	if (thread->lockCount == 0)
 	{
-		thread->hasReadLock = false;
-		thread->hasWriteLock = false;
+		[self freeLockState:[NSThread currentThread]];
 		pthread_rwlock_unlock(&lock);
 	}
 }
 
 - (ThreadLockState*)currentThreadState
 {
-	ThreadLockState* state = [[self threadDict] valueForKey:@"lockState"];
-	if (state == nil)
+	NSThread* currentThread = [NSThread currentThread];
+	NSValue* key = [NSValue valueWithPointer:currentThread];
+	
+	ThreadLockState* state;
+	@synchronized(threadLockStates) {
+		state = [threadLockStates objectForKey:key];
+	}
+	
+	if (state == nil) // Create and store thread state object
 	{
 		state = [[[ThreadLockState alloc] init] autorelease];
-		[[self threadDict] setValue:state forKey:@"lockState"];
+		@synchronized(threadLockStates) {
+			[threadLockStates setObject:state forKey:key];
+		}
+		
+#ifdef CHECK_LOCK_RELEASE
+		[[NSNotificationCenter defaultCenter]
+			 addObserver: self
+			 selector: @selector(onThreadExit:)
+			 name: NSThreadWillExitNotification
+			 object: currentThread];
+#endif
 	}
 	return state;
 }
 
-- (NSMutableDictionary*)threadDict
+- (void)freeLockState:(NSThread*)thread
 {
-	return [[NSThread currentThread] threadDictionary];
+	@synchronized(threadLockStates)	{
+		[threadLockStates removeObjectForKey:[NSValue valueWithPointer:thread]];
+	}
 }
+
+#pragma mark Debugging
+
+#ifdef CHECK_LOCK_RELEASE
+- (void)onThreadExit:(NSNotification*)notification
+{
+	NSThread* thread = [notification object];
+	ThreadLockState* state;
+	@synchronized(threadLockStates) {
+		 state = [threadLockStates objectForKey: [NSValue valueWithPointer:thread]];
+	}
+	
+	if (state != nil && state->lockCount != 0)
+	{
+		[NSException raise:@"LockNotReleased" format:@"thread %@ has exited without releasing the lock", thread];
+	}
+}
+#endif
+
+#ifdef CHECK_LOCK_STATE
+- (void)checkLockState:(ThreadLockState*)state
+{
+	NSAssert(state != nil, @"[RWLock self check] state is nil");
+	if (state->hasReadLock && state->hasWriteLock)
+		NSAssert(state->lockCount >= 2, @"[RWLock self check]: lockCount should be >= 2 because hasReadLock = true and hasWriteLock = true");
+	else if (state->hasReadLock)
+		NSAssert(state->lockCount >= 1, @"[RWLock self check]: lockCount should be >= 1 because hasReadLock = true");
+	else if (state->hasWriteLock)
+		NSAssert(state->lockCount >= 1, @"[RWLock self check]: lockCount should be >= 1 because hasWriteLock = true");
+}
+#endif
 
 @end
